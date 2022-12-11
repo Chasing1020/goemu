ee10b3021eb4e327b99bbd7ac4327595558a8871
Chasing1020 <chasing1020@gmail.com>
2022-12-11 10:05:16 +0800

    change the memory data size to bytes

--- runtime/memory.go
+++ runtime/memory.go
@@ -7,36 +7,36 @@ import (
 
 type Memory []uint8
 
-func (m *Memory) Check(addr, size uint64) error {
+func (m *Memory) Check(addr, bytes uint64) error {
 	if addr < config.KernelBase || addr > config.KernelEnd {
 		return fmt.Errorf("invalid memory address: %x", addr)
 	}
-	switch size {
-	case 8, 16, 32, 64:
+	switch bytes {
+	case 1, 2, 4, 8:
 		return nil
 	default:
-		return fmt.Errorf("invalid data size: %d", size)
+		return fmt.Errorf("invalid data bytes: %d", bytes)
 	}
 }
 
-func (m *Memory) Load(addr, size uint64) (uint64, error) {
-	if err := m.Check(addr, size); err != nil {
+func (m *Memory) Load(addr, bytes uint64) (uint64, error) {
+	if err := m.Check(addr, bytes); err != nil {
 		return 0, err
 	}
 	index := addr - config.KernelBase
 	data := uint64((*m)[index])
-	for i := uint64(1); i < size/8; i++ {
+	for i := uint64(1); i < bytes; i++ {
 		data |= uint64((*m)[index+i]) << (i * 8)
 	}
 	return data, nil
 }
 
-func (m *Memory) Store(addr, size uint64, data uint64) error {
-	if err := m.Check(addr, size); err != nil {
+func (m *Memory) Store(addr, bytes uint64, data uint64) error {
+	if err := m.Check(addr, bytes); err != nil {
 		return err
 	}
 	index := addr - config.KernelBase
-	for i := uint64(0); i < size/8; i++ {
+	for i := uint64(0); i < bytes; i++ {
 		offset := 8 * i
 		(*m)[index+i] = uint8((data >> offset) & 0xff)
 	}
